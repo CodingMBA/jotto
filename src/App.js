d326fcda8885d3e2c464ee7268b28384d5675e7e
Andrew Smith <codingmba@gmail.com>
2019-08-16 19:01:08 -0400

    adds bootstrap styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import GuessedWords from './GuessedWords';
+import Congrats from './Congrats';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1>Jotto</h1>
+      <Congrats success={true} />
+      <GuessedWords
+        guessedWords={[{ guessedWord: 'train', letterMatchCount: 3 }]}
+      />
     </div>
   );
 }
--- src/Congrats.js
+++ src/Congrats.js
@@ -1,6 +1,5 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-
+import React from 'react';
+import PropTypes from 'prop-types';
 
 /**
  * Functional react component for congratulatory message
@@ -8,26 +7,22 @@ import PropTypes from 'prop-types'
  * @params {object} props - React.props from App component
  * @returns {JSX.Element} - Rendered component (or null if 'success' prop is present)
  */
-const Congrats = (props) => {
+const Congrats = props => {
   if (props.success) {
     return (
-      <div data-test="component-congrats">
-        <span data-test="congrats-message">
+      <div data-test='component-congrats' className='alert alert-success'>
+        <span data-test='congrats-message'>
           Congratulations! You guessed the word!
         </span>
       </div>
-    )
+    );
   } else {
-    return (
-      <div data-test="component-congrats"/>
-    )
+    return <div data-test='component-congrats' />;
   }
-
-  
-}
+};
 
 Congrats.propTypes = {
-  success: PropTypes.bool.isRequired,
-}
+  success: PropTypes.bool.isRequired
+};
 
-export default Congrats
+export default Congrats;
