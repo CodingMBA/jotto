b7b00a7106409d3ebf1030ecc3777d1bf84e2b7a
Andrew Smith <codingmba@gmail.com>
2019-08-18 16:36:07 -0400

    Connects App component and adds tests

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import './App.css';
 import GuessedWords from './GuessedWords';
 import Congrats from './Congrats';
+import Input from './Input';
+import { getSecretWord } from './actions';
 
-function App() {
-  return (
-    <div className='container'>
-      <h1>Jotto</h1>
-      <Congrats success={true} />
-      <GuessedWords
-        guessedWords={[{ guessedWord: 'train', letterMatchCount: 3 }]}
-      />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className='container'>
+        <h1>Jotto</h1>
+        <Congrats success={this.props.success} />
+        <Input />
+        <GuessedWords guessedWords={this.props.guessedWords} />
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = state => {
+  const { success, guessedWords, secretWord } = state;
+  return { success, guessedWords, secretWord };
+};
+
+export default connect(
+  mapStateToProps,
+  { getSecretWord }
+)(App);
